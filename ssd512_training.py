ec32cd4932ef8139eaabb500645c663abf57b272
LongChenCV <40469503+LongChenCV@users.noreply.github.com>
2020-07-27 23:29:20 +0100

    Add files via upload

--- ssd512_training.py
+++ ssd512_training.py
@@ -15,17 +15,15 @@ img_width = 512
 img_channels = 3
 mean_color = [123, 117, 104]
 swap_channels = [2, 1, 0]
-n_classes = 3
-scales_pascal = [0.04, 0.07, 0.15, 0.3, 0.45, 0.6]
+n_classes = 4
+scales_pascal = [0.3, 0.15, 0.07, 0.04]
 scales = scales_pascal
-aspect_ratios = [[1.0, 2.0, 0.5],
-                 [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
-                 [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
+aspect_ratios = [[1.0, 2.0, 0.5, 3.0, 1.0/3.0],
                  [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
                  [1.0, 2.0, 0.5, 3.0, 1.0/3.0]]
 two_boxes_for_ar1 = True
-steps = [4, 8, 16, 32, 64]
-offsets = [0.5, 0.5, 0.5, 0.5, 0.5]
+steps = [16, 8, 4]
+offsets = [0.5, 0.5, 0.5]
 clip_boxes = False
 variances = [0.1, 0.1, 0.2, 0.2]
 normalize_coords = True
@@ -52,15 +50,17 @@ adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 ssd_loss = SSDLoss(neg_pos_ratio=3, alpha=1.0)
 model.compile(optimizer=adam, loss=ssd_loss.compute_loss)
 
-train_dataset = DataGenerator(load_images_into_memory=False, hdf5_dataset_path=None)
-val_dataset = DataGenerator(load_images_into_memory=False, hdf5_dataset_path=None)
-VOC_2013_images_dir = '/data/deeplearn/SWEIPENet/dataset/JPEGImages/'
-VOC_2013_annotations_dir = '/data/deeplearn/SWEIPENet/dataset/Annotations/'
-VOC_2013_sampleweights_dir='/data/deeplearn/SWEIPENet/dataset/'
-VOC_2013_trainval_image_set_filename = '/data/deeplearn/SWEIPENet/dataset/ImageSets/Main/trainval.txt'
-VOC_2013_test_image_set_filename = '/data/deeplearn/SWEIPENet/dataset/ImageSets/Main/test.txt'
+# Whether need sample weights
+VOC_2013_sampleweights_dir=None
+# VOC_2013_sampleweights_dir = '/data/deeplearn/VOCdevkit/VOC2013/Weights/'
+VOC_2013_images_dir = '/data/deeplearn/VOCdevkit/URPC2018/JPEGImages/'
+VOC_2013_annotations_dir = '/data/deeplearn/VOCdevkit/URPC2018/Annotations/'
+VOC_2013_trainval_image_set_filename = '/data/deeplearn/VOCdevkit/URPC2018/ImageSets/Main/trainval.txt'
+VOC_2013_test_image_set_filename = '/data/deeplearn/VOCdevkit/URPC2018/ImageSets/Main/test.txt'
+classes = ['background', 'seacucumber', 'seaurchin', 'scallop', 'starfish']
 
-classes = ['background', 'seacucumber', 'seaurchin', 'scallop']
+train_dataset = DataGenerator(load_images_into_memory=True, hdf5_dataset_path=None)
+val_dataset = DataGenerator(load_images_into_memory=True, hdf5_dataset_path=None)
 train_dataset.parse_xml(images_dirs=[VOC_2013_images_dir],
                         image_set_filenames=[VOC_2013_trainval_image_set_filename],
                         sample_weights_dirs=VOC_2013_sampleweights_dir,
@@ -78,16 +78,19 @@ val_dataset.parse_xml(images_dirs=[VOC_2013_images_dir],
                       exclude_truncated=False,
                       exclude_difficult=True,
                       ret=False)
-train_dataset.create_hdf5_dataset(file_path='dataset_pascal_voc_2013_trainval.h5',
+train_dataset.create_hdf5_dataset(file_path='dataset_pascal_voc_URPC2018_trainval.h5',
                                   resize=False,
                                   variable_image_size=True,
                                   verbose=True)
-val_dataset.create_hdf5_dataset(file_path='dataset_pascal_voc_2013_test.h5',
+val_dataset.create_hdf5_dataset(file_path='dataset_pascal_voc_URPC2018_test.h5',
                                 resize=False,
                                 variable_image_size=True,
                                 verbose=True)
 
-batch_size = 4
+# train_dataset = DataGenerator(load_images_into_memory=True, hdf5_dataset_path='/data/deeplearn/ssd_keras_sampleweight/dataset_pascal_voc_URPC2018_trainval.h5')
+# val_dataset = DataGenerator(load_images_into_memory=True, hdf5_dataset_path='/data/deeplearn/ssd_keras_sampleweight/dataset_pascal_voc_URPC2018_test.h5')
+
+batch_size = 16
 # For the training generator:
 ssd_data_augmentation = SSDDataAugmentation(img_height=img_height,
                                             img_width=img_width,
@@ -96,11 +99,9 @@ ssd_data_augmentation = SSDDataAugmentation(img_height=img_height,
 convert_to_3_channels = ConvertTo3Channels()
 resize = Resize(height=img_height, width=img_width)
 # 5: Instantiate an encoder that can encode ground truth labels into the format needed by the SSD loss function.
-predictor_sizes = [model.get_layer('deconv3_2_mbox_conf').output_shape[1:3],
-                   model.get_layer('deconv4_2_mbox_conf').output_shape[1:3],
-                   model.get_layer('deconv5_2_mbox_conf').output_shape[1:3],
-                   model.get_layer('deconv6_2_mbox_conf').output_shape[1:3],
-                   model.get_layer('conv7_add_mbox_conf').output_shape[1:3]]
+predictor_sizes = [model.get_layer('deconv1_mbox_conf').output_shape[1:3],
+                   model.get_layer('deconv2_mbox_conf').output_shape[1:3],
+                   model.get_layer('deconv3_mbox_conf').output_shape[1:3]]
 ssd_input_encoder = SSDInputEncoder(img_height=img_height,
                                     img_width=img_width,
                                     n_classes=n_classes,
@@ -122,8 +123,7 @@ train_generator = train_dataset.generate(batch_size=batch_size,
                                          transformations=[ssd_data_augmentation],
                                          label_encoder=ssd_input_encoder,
                                          returns={'processed_images',
-                                                  'encoded_labels',
-                                                  'sample_weights'},
+                                                  'encoded_labels'},
                                          keep_images_without_gt=False)
 val_generator = val_dataset.generate(batch_size=batch_size,
                                      shuffle=False,
@@ -141,21 +141,23 @@ print("Number of images in the validation dataset:\t{:>6}".format(val_dataset_si
 
 # Define a learning rate schedule.
 def lr_schedule(epoch):
-    if epoch < 120:
+    if epoch < 80:
+        return 0.001
+    elif epoch<120:
         return 0.0001
-    # elif epoch < 70:
-    #     return 0.001
+    else:
+        return  0.0001
 
 # Define model callbacks.
 # TODO: Set the filepath under which you want to save the model.
-model_checkpoint = ModelCheckpoint(filepath='ssd512_2013_adam16_0.0001_time3_epoch-{epoch:02d}_loss-{loss:.4f}_val_loss-{val_loss:.4f}.h5',
+model_checkpoint = ModelCheckpoint(filepath='ssd512_URPC2018_epoch-{epoch:02d}.h5',
                                    monitor='val_loss',
                                    verbose=1,
                                    save_best_only=False, # True
                                    save_weights_only=False,
                                    mode='auto',
                                    period=1)
-csv_logger = CSVLogger(filename='ssd512_2013_adam16_0.0001_time3_training_log.csv',
+csv_logger = CSVLogger(filename='ssd512_URPC2018_training_log_7_21.csv',
                        separator=',',
                        append=True)
 learning_rate_scheduler = LearningRateScheduler(schedule=lr_schedule,
@@ -168,10 +170,11 @@ callbacks = [model_checkpoint,
 
 # If you're resuming a previous training, set `initial_epoch` and `final_epoch` accordingly.
 initial_epoch = 0
-final_epoch = 120
-steps_per_epoch = 500
+final_epoch = 220
+steps_per_epoch = 1000
 
 history = model.fit_generator(generator=train_generator,
+                              class_weight='auto',
                               steps_per_epoch=steps_per_epoch,
                               epochs=final_epoch,
                               callbacks=callbacks,
